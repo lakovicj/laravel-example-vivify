6a5ac72596ae7abc5dbd375eb1635fb2e9a0e880
lakovicj <31940189+lakovicj@users.noreply.github.com>
2020-11-16 12:04:09 +0100

    Simple blade template (#3)
    Co-authored-by: Jovan Lakovic <jovan.lakovic@vivifyideas.com>

--- laravel-example/routes/web.php
+++ laravel-example/routes/web.php
@@ -18,33 +18,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', function() {
-    return response('Testing GET method route...', 200);
-})->name('get');
 
-Route::get('/test/{id}', function($id) {
-    return response("Getting entity with id=$id", 200);
-})->name('getOne');
+Route::get('/simple', function() {
+    return view('simple', ['first_name' => 'Jovan']);
+});
 
-
-Route::post('/test', function(Request $request) {
-
-    if ($request->has('name')) {
-        return Response("Creating new entity with param name=$request->name", 201);
-    }
-    return response("Creating new entity....", 201);
-})->name('create');
-
-Route::patch('/test', function() {
-    return response("Patch", 200);
-})->name('patch');
-
-Route::put('/test/{id}', function($id, Request $request) {
-    // we can access request params through $request
-    return response("Entity with id=$id is being edited.");
-})->name('edit');
-
-Route::delete('/test/{id}', function($id) {
-    return response("Deleting entity with id=$id", 200);
-})->name('remove');
 
